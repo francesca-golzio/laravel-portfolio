9ae8c18d5db7518394c626282e1c2ea86c52a23d
francesca-golzio <francescagolzio@gmail.com>
2026-08-10 18:38:09 +0200

    add Project Model
    - add the Project Model
    - add the the project migration
    - add the project seeder (sample data)
    - call the project seeder from the database seeder

---
+++ database/seeders/ProjectsTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class ProjectsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {        
+        for($i = 0; $i < 6; $i++) {
+            $newProject = new Project();
+
+            $newProject->title = $faker->sentence(3);
+            $newProject->client = $faker->name() . ' ' . $faker->lastName() . ' - ' . $faker->company();
+            $newProject->period = $faker->monthName() . ' ' . $faker->year();
+            $newProject->description = "{$faker->paragraph(14)}";
+
+            $newProject->save();
+        }
+    }
+}
