5018fdc751d6f39ed26a3b0ff464a0f8f583816b
francesca-golzio <francescagolzio@gmail.com>
2026-08-14 17:42:28 +0200

    update the pojects/show view

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -7,7 +7,7 @@
 
 
 <div class="table-responsive" >
-  <table class="table table-dark table-striped table-borderless fs-4 w100">
+  <table class="table table-dark table-striped table-borderless fs-5 w100">
     <thead>
       <tr class="text-center">
         <th scope="row" colspan="3">
@@ -39,14 +39,23 @@
         <td><div>{{ $project->description }}</div></td>
         <td><button><i class="bi bi-pencil-square" title="modify"></i></button></td>
       </tr>
+      <!-- Risorse ?? -->
       <tr class="">
-        <td scope="row"><small class="accent_color">Risorse</small></td>
-        <td></td>
+        <td scope="row"><small class="accent_color">Tags</small></td>
+        <td>
+          <div class="d-flex gap-4 fs-5">
+            @foreach ($project->tags as $tag)
+            <div>
+              <span class="rounded-pill fs-6 px-2 py-1 accent_border">{{ $tag }}</span>
+            </div>
+            @endforeach
+          </div>
+        </td>
         <td><button><i class="bi bi-pencil-square" title="modify"></i></button></td>
       </tr>
       <tr class="">
         <td scope="row"><small class="accent_color"><i class="bi bi-lock-fill"></i> Note</small></td>
-        <td><div></div></td>
+        <td><div>{{ $project->personal_note }}</div></td>
         <td><button><i class="bi bi-pencil-square" title="modify"></i></button></td>
       </tr>
     </tbody>
