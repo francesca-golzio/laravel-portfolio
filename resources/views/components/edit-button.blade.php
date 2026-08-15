acf798d1a0dc10194cd3d7146c01618bf3c310a7
francesca-golzio <francescagolzio@gmail.com>
2026-08-15 12:17:42 +0200

    add edit and show buttons components

---
+++ resources/views/components/edit-button.blade.php
@@ -0,0 +1,3 @@
+<a href="{{ route('projects.edit', $project) }}"class="btn edit_button" title="edit">
+    <i class="bi bi-pencil-square"></i>
+</a>
---
+++ resources/views/components/show-button.blade.php
@@ -0,0 +1,3 @@
+<a href="{{ route('projects.show', $project) }}"class="btn show_button" title="show">
+    <i class="bi bi-arrows-angle-expand"></i>
+</a>
