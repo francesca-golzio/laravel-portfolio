f3cc32c0b580e45278d3d16223b80065bc74ca52
francesca-golzio <francescagolzio@gmail.com>
2026-08-18 12:43:55 +0200

    revert "add the Tecnology Model"
    This reverts commit 2a5a2a36f5c60379193d1a69c1232471c4d6c3ac.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(TypesTableSeeder::class);
-        $this->call(TechnologiesTableSeeder::class);
         $this->call(ProjectsTableSeeder::class);
     }
 }
