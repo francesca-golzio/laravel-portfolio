0010bd5cc47eab5e37f5515f32d3deaf45272990
francesca-golzio <francescagolzio@gmail.com>
2026-08-20 12:02:39 +0200

    add api/projects (index)

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    public function index() {
+        
+       //  $projects = Project::all();
+        $projects = Project::with('technologies', 'type')->get();
+
+        return response()->json(
+            [
+                'success' => true,
+                'data' => $projects,
+            ]
+        );
+    }
+}
